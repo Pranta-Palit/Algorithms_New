918d79254f7492b5fc6db4bb7008755ff1a5b67c
Hasan Mahmud <hasan.357cse@gmail.com>
2016-11-24 21:21:27 +0600

    Insertion Sort

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -1,34 +1,68 @@
-///................................................Code...................................................................
+
 #include<bits/stdc++.h>
-using namespace std;
-#define MAX 1000
-int arr[MAX];
+#define arr_size 100
+
+int arr[arr_size];
+int temp[arr_size], counter = 0;
+
+void merging(int startPoint, int midPoint, int endPoint)
+{
+    int firstArrCnt,secArrCnt,i;
+    firstArrCnt = startPoint;
+    secArrCnt = midPoint + 1;
+
+    for(i = firstArrCnt; firstArrCnt<=midPoint && secArrCnt<=endPoint; i++)
+    {
+        if(arr[firstArrCnt] < arr[secArrCnt])
+            temp[i] = arr[firstArrCnt++];
+        else
+            temp[i] = arr[secArrCnt++];
+    }
+
+    while(firstArrCnt <= midPoint)
+        temp[i++] = arr[firstArrCnt++];
+
+    while(secArrCnt <= endPoint)
+        temp[i++] = arr[secArrCnt++];
+
+    for(i = startPoint; i<=endPoint; i++)
+        arr[i] = temp[i];
+
+}
+
+void sorting(int startPoint, int endPoint)
+{
+    int midPoint;
+
+    if(startPoint >= endPoint)
+        return;
+
+    printf("Counter: %d: Start: %d, End: %d\n",++counter, startPoint, endPoint);
+
+    midPoint = (startPoint+endPoint)/2;
+
+    sorting(startPoint, midPoint);
+    sorting(midPoint+1, endPoint);
+
+    merging(startPoint, midPoint, endPoint);
+}
+
 int main()
 {
-///.............................................Variable................................................................
-    int n,element,j;
-    scanf("%d",&n); /// How many numbers input in array
-///.............................................Input in Array......................................................
-    for(int i=0; i<n; i++)
-        scanf("%d",&arr[i]);
-///............................................Sorting.................................................................
-    for(int i=0; i<n; i++)
-    {
+    int i, n;
 
-        element = arr[i];
-        j=i;
+    printf("Enter number of elements:\n");
+    scanf("%d", &n);
+    printf("Enter %d elements:\n",n);
+    for(i=0; i<n; i++)
+        scanf("%d", &arr[i]);
 
-        while(j>0 && arr[j-1]>element)
-        {
-            arr[j] = arr[j-1];
-            j = j-1;
-        }
-        arr[j] = element;
-    }
-///.........................................................Output...................................................
-    for(int k=0; k<n; k++)
-        printf("%d ",arr[k]);
-    printf("\n");
-///.........................................................END........................................................
+    sorting(0, n-1);
+
+    printf("\nAfter sorting the Array:\n");
+    for(i = 0; i<n; i++)
+        printf("%d ", arr[i]);
+    puts("");
+
     return 0;
 }
